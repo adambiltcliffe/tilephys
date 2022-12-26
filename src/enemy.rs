023922c1fce99011865adbe8b84de299f33f4c46
Adam Biltcliffe <adam.biltcliffe@gmail.com>
2022-12-26 21:12:40 +0000

    removed Enemy component and replaced with per-enemy behaviours

--- src/enemy.rs
+++ src/enemy.rs
@@ -12,24 +12,6 @@ pub enum EnemyKind {
     SpiderParrot,
 }
 
-impl EnemyKind {
-    fn jump_prob(&self) -> f32 {
-        match self {
-            EnemyKind::Dog => 0.45, // lots of small jumps
-            EnemyKind::JumpyDog => 0.2,
-            EnemyKind::SpiderParrot => 0.0,
-        }
-    }
-
-    fn jump_vel(&self) -> f32 {
-        match self {
-            EnemyKind::Dog => -6.0,
-            EnemyKind::JumpyDog => -8.0,
-            EnemyKind::SpiderParrot => 0.0,
-        }
-    }
-}
-
 pub fn add_enemy(world: &mut World, kind: EnemyKind, x: i32, y: i32) {
     let h = match kind {
         EnemyKind::SpiderParrot => 24,
@@ -37,26 +19,25 @@ pub fn add_enemy(world: &mut World, kind: EnemyKind, x: i32, y: i32) {
     };
     let rect = IntRect::new(x - 12, y - h, 24, h);
     let actor = Actor::new(&rect, 0.4);
-    let enemy = Enemy::new();
     let hittable = EnemyHittable::new(3);
     let dmg = EnemyContactDamage::new();
     if kind == EnemyKind::SpiderParrot {
         world.spawn((
             kind,
+            ParrotBehaviour::new(),
             rect,
             crate::draw::ParrotSprite::new(),
             actor,
-            enemy,
             hittable,
             dmg,
         ));
     } else {
         world.spawn((
             kind,
+            DogBehaviour::new(),
             rect,
             crate::draw::DogSprite::new(),
             actor,
-            enemy,
             hittable,
             dmg,
         ));
@@ -96,84 +77,115 @@ impl EnemyContactDamage {
     }
 }
 
-struct Enemy {
+struct DogBehaviour {
     dir: f32,
     jump_y: Option<i32>,
 }
 
-impl Enemy {
+impl DogBehaviour {
     pub fn new() -> Self {
         Self {
             dir: 0.0,
             jump_y: None,
         }
     }
+
+    pub fn update(world: &World, resources: &Resources) {
+        let player_x = player_x(world, resources.player_id);
+        for (_, (kind, actor, enemy, rect, spr)) in world
+            .query::<(
+                &EnemyKind,
+                &mut Actor,
+                &mut DogBehaviour,
+                &IntRect,
+                &mut DogSprite,
+            )>()
+            .iter()
+        {
+            if (actor.grounded || enemy.jump_y.is_some()) && with_prob(0.1) {
+                if player_x.is_some() && with_prob(0.7) {
+                    enemy.dir = (player_x.unwrap() - rect.centre().x).signum() * 5.0;
+                } else {
+                    enemy.dir = 5.0 * rand_sign();
+                }
+            }
+            if actor.grounded {
+                let (jump_prob, jump_vel) = match kind {
+                    EnemyKind::Dog => (0.45, -6.0),
+                    EnemyKind::JumpyDog => (0.2, -8.0),
+                    _ => unreachable!(),
+                };
+                if with_prob(jump_prob) {
+                    actor.vy = jump_vel;
+                    enemy.jump_y = Some(rect.y);
+                } else {
+                    enemy.jump_y = None;
+                }
+            } else {
+                // stop moving horizontally if ground has fallen out from under us
+                if match enemy.jump_y {
+                    None => true,
+                    Some(y) => y < rect.y,
+                } {
+                    enemy.dir = 0.0;
+                }
+            }
+            actor.vx += enemy.dir;
+            if actor.vx < 0.0 {
+                spr.flipped = false
+            }
+            if actor.vx > 0.0 {
+                spr.flipped = true
+            }
+            spr.n += 1;
+        }
+    }
+}
+struct ParrotBehaviour {
+    dir: f32,
+}
+
+impl ParrotBehaviour {
+    pub fn new() -> Self {
+        Self { dir: 0.0 }
+    }
+
+    pub fn update(world: &World, resources: &Resources) {
+        let player_x = player_x(world, resources.player_id);
+        for (_, (actor, enemy, rect, spr)) in world
+            .query::<(
+                &mut Actor,
+                &mut ParrotBehaviour,
+                &IntRect,
+                &mut ParrotSprite,
+            )>()
+            .iter()
+        {
+            if actor.grounded && with_prob(0.1) {
+                if player_x.is_some() && with_prob(0.7) {
+                    enemy.dir = (player_x.unwrap() - rect.centre().x).signum() * 5.0;
+                } else {
+                    enemy.dir = 5.0 * rand_sign();
+                }
+            }
+            if !actor.grounded {
+                enemy.dir = 0.0;
+            }
+            actor.vx += enemy.dir;
+            if actor.vx < 0.0 {
+                spr.flipped = false
+            }
+            if actor.vx > 0.0 {
+                spr.flipped = true
+            }
+            spr.n += 1;
+        }
+    }
 }
 
 pub fn update_enemies(world: &World, resources: &Resources) {
-    let player_x = player_x(world, resources.player_id);
-    // TODO this is super bad, look at all this duplication
-
-    for (_, (kind, actor, enemy, rect, spr)) in world
-        .query::<(&EnemyKind, &mut Actor, &mut Enemy, &IntRect, &mut DogSprite)>()
-        .iter()
-    {
-        if (actor.grounded || enemy.jump_y.is_some()) && with_prob(0.1) {
-            if player_x.is_some() && with_prob(0.7) {
-                enemy.dir = (player_x.unwrap() - rect.centre().x).signum() * 5.0;
-            } else {
-                enemy.dir = 5.0 * rand_sign();
-            }
-        }
-        if actor.grounded {
-            if with_prob(kind.jump_prob()) {
-                actor.vy = kind.jump_vel();
-                enemy.jump_y = Some(rect.y);
-            } else {
-                enemy.jump_y = None;
-            }
-        } else {
-            // stop moving horizontally if ground has fallen out from under us
-            if match enemy.jump_y {
-                None => true,
-                Some(y) => y < rect.y,
-            } {
-                enemy.dir = 0.0;
-            }
-        }
-        actor.vx += enemy.dir;
-        if actor.vx < 0.0 {
-            spr.flipped = false
-        }
-        if actor.vx > 0.0 {
-            spr.flipped = true
-        }
-        spr.n += 1;
-    }
-
-    for (_, (actor, enemy, rect, spr)) in world
-        .query::<(&mut Actor, &mut Enemy, &IntRect, &mut ParrotSprite)>()
-        .iter()
-    {
-        if actor.grounded && with_prob(0.1) {
-            if player_x.is_some() && with_prob(0.7) {
-                enemy.dir = (player_x.unwrap() - rect.centre().x).signum() * 5.0;
-            } else {
-                enemy.dir = 5.0 * rand_sign();
-            }
-        }
-        if !actor.grounded {
-            enemy.dir = 0.0;
-        }
-        actor.vx += enemy.dir;
-        if actor.vx < 0.0 {
-            spr.flipped = false
-        }
-        if actor.vx > 0.0 {
-            spr.flipped = true
-        }
-        spr.n += 1;
-    }
+    DogBehaviour::update(world, resources);
+    ParrotBehaviour::update(world, resources);
 
     for (_, (_, rect)) in world.query::<(&EnemyContactDamage, &IntRect)>().iter() {
         if let Ok(mut q) = world.query_one::<(&mut Controller, &IntRect)>(resources.player_id) {
