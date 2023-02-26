c8a446ce609e4b566d52580f3c245eceb31e8c5c
Adam Biltcliffe <adam.biltcliffe@gmail.com>
2023-02-26 21:09:50 +0000

    config module now macro-generated (slightly awkward and not working in release mode)

--- src/config.rs
+++ src/config.rs
@@ -1,73 +1,94 @@
 #[cfg(debug_assertions)]
-mod dynamic_config {
-    use once_cell::sync::Lazy;
-    use rhai::plugin::*;
-    use rhai::{def_package, export_module};
-    use std::sync::{Mutex, MutexGuard};
+use paste::paste;
 
-    pub struct DynamicConfig {
-        gravity: f32,
+/*#[cfg(debug_assertions)]
+macro_rules! make_setter_getter {
+    ($name:ident, $str_name:tt, $typ:ty) => {
+
     }
-
-    impl DynamicConfig {
-        fn new() -> Self {
-            Self { gravity: 1.0 }
-        }
-
-        pub fn gravity(&self) -> f32 {
-            self.gravity
-        }
-    }
-
-    static INSTANCE: Lazy<Mutex<DynamicConfig>> = Lazy::new(|| Mutex::new(DynamicConfig::new()));
-
-    pub fn config() -> MutexGuard<'static, DynamicConfig> {
-        INSTANCE.try_lock().unwrap()
-    }
-
-    #[derive(Clone)]
-    pub struct ConfigProxy {}
-
-    #[export_module]
-    mod config_interface {
-        #[rhai_fn(get = "gravity")]
-        pub fn get_gravity(_this: &mut ConfigProxy) -> f32 {
-            config().gravity
-        }
-
-        #[rhai_fn(set = "gravity")]
-        pub fn set_gravity(_this: &mut ConfigProxy, val: f32) {
-            config().gravity = val;
-        }
-    }
-
-    def_package! {
-        pub ConfigPackage(module) {
-            combine_with_exported_module!(module, "config-mod", config_interface);
-        } |> |engine| {
-            engine.register_type_with_name::<ConfigProxy>("Config");
-        }
-    }
-}
+}*/
 
 #[cfg(debug_assertions)]
-pub use dynamic_config::*;
+macro_rules! make_config {
+    [$(($name:ident, $str_name:tt, $typ:ty, $val:tt)),*] => {
+        paste! {
+            use once_cell::sync::Lazy;
+            use rhai::plugin::*;
+            use rhai::{def_package, export_module};
+            use std::sync::{Mutex, MutexGuard};
 
-#[cfg(not(debug_assertions))]
-mod fixed_config {
-    pub struct FixedConfig {}
+            pub struct DynamicConfig {
+                $($name: $typ, )*
+            }
 
-    impl FixedConfig {
-        #[inline(always)]
-        pub fn gravity(&self) -> f32 {
-            1.0
+            impl DynamicConfig {
+                fn new() -> Self {
+                    Self {
+                        $($name: $val, )*
+                    }
+                }
+
+                $(
+                    pub fn $name(&self) -> $typ {
+                        self.$name
+                    }
+                )*
+            }
+
+            static INSTANCE: Lazy<Mutex<DynamicConfig>> =
+                Lazy::new(|| Mutex::new(DynamicConfig::new()));
+
+            pub fn config() -> MutexGuard<'static, DynamicConfig> {
+                INSTANCE.try_lock().unwrap()
+            }
+
+            #[derive(Clone)]
+            pub struct ConfigProxy {}
+
+            #[export_module]
+            mod config_interface {
+
+                    $(
+                        #[rhai_fn(get = $str_name)]
+                        pub fn [<get_ $name>](_this: &mut ConfigProxy) -> $typ {
+                            config().$name
+                        }
+
+                        #[rhai_fn(set = $str_name)]
+                        pub fn [<set_ $name>](_this: &mut ConfigProxy, val: $typ) {
+                            config().$name = val;
+                        }
+                    )*
+
+            }
+
+            def_package! {
+                pub ConfigPackage(module) {
+                    combine_with_exported_module!(module, "config-mod", config_interface);
+                } |> |engine| {
+                    engine.register_type_with_name::<ConfigProxy>("Config");
+                }
+            }
         }
-    }
-
-    pub fn config() -> FixedConfig {
-        FixedConfig {}
     }
 }
 
 #[cfg(not(debug_assertions))]
-pub use fixed_config::*;
+macro_rules! make_config {
+    () => {
+        pub struct FixedConfig {}
+
+        impl FixedConfig {
+            #[inline(always)]
+            pub fn gravity(&self) -> f32 {
+                1.0
+            }
+        }
+
+        pub fn config() -> FixedConfig {
+            FixedConfig {}
+        }
+    };
+}
+
+make_config![(gravity, "gravity", f32, 1.0)];
